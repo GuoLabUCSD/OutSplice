65dbc1b577b2d8bd9c66297d8d203453f223d2bb
GuoLabUCSD <108438457+GuoLabUCSD@users.noreply.github.com>
2022-12-02 12:44:02 -0800

    Add files via upload

--- OutSplice/SpliceBurdenfunction.R
+++ OutSplice/SpliceBurdenfunction.R
@@ -24,14 +24,9 @@ CalcBurden <- function(junc.Outliers, ASE.type, FisherAnalyses, p_value) {
   Over_Expressed_Calls_with_Type <- na.omit(Over_Expressed_Calls_with_Type)
   Over_Expressed_Calls_with_Type <- subset(Over_Expressed_Calls_with_Type, Over_Expressed_Calls_with_Type$FisherP2 < p_value)
   Over_Expressed_Calls_with_Type <- subset(Over_Expressed_Calls_with_Type, select = -FisherP2)
-
-  Number_overexpressed_per_tumor <- sapply(Over_Expressed_Calls_with_Type, table)
-  Number_overexpressed_per_tumor <- as.data.frame(Number_overexpressed_per_tumor)
-
-  transposed_Over <- t(Number_overexpressed_per_tumor)
-  colnames(transposed_Over) <- c('NoOutlierNumber', 'OutlierNumberOver')
-  transposed_Over <- subset(transposed_Over, select = -NoOutlierNumber)
-  Over_Expressed_Splicing_Burden <- as.data.frame(transposed_Over)
+  
+  OutlierNumberOver = colSums(Over_Expressed_Calls_with_Type)
+  all_oe_results_df = data.frame(OutlierNumberOver)
 
   #Calculate Splicing Burden for Significant Events Under-Expressed in Tumors
   Under_Expressed_Junctions_Outlier_Calls <- as.data.frame(junc.Outliers$TumorUnderExpression)
@@ -53,22 +48,17 @@ CalcBurden <- function(junc.Outliers, ASE.type, FisherAnalyses, p_value) {
   Under_Expressed_Calls_with_Type <- na.omit(Under_Expressed_Calls_with_Type)
   Under_Expressed_Calls_with_Type <- subset(Under_Expressed_Calls_with_Type, Under_Expressed_Calls_with_Type$FisherP1 < p_value)
   Under_Expressed_Calls_with_Type <- subset(Under_Expressed_Calls_with_Type, select = -FisherP1)
-
-  Number_underexpressed_per_tumor <- sapply(Under_Expressed_Calls_with_Type, table)
-  Number_underexpressed_per_tumor <- as.data.frame(Number_underexpressed_per_tumor)
-
-  transposed_Under <- t(Number_underexpressed_per_tumor)
-  colnames(transposed_Under) <- c('NoOutlierNumber', 'OutlierNumberUnder')
-  transposed_Under <- subset(transposed_Under, select = -NoOutlierNumber)
-  Under_Expressed_Splicing_Burden <- as.data.frame(transposed_Under)
-
+  
+  OutlierNumberUnder = colSums(Under_Expressed_Calls_with_Type)
+  all_ue_results_df = data.frame(OutlierNumberUnder)
+  
   #Total Splicing Burden as the sum of Over + Under Expressed Events
-  Total_Outliers <- transform(merge(transposed_Over, transposed_Under, by = 0, all = TRUE), TotalOutliers = OutlierNumberOver + OutlierNumberUnder)
-  rownames(Total_Outliers) <- Total_Outliers[, 1]
-  transposed_Total <- subset(Total_Outliers, select = c(-OutlierNumberOver, -OutlierNumberUnder, -Row.names))
-  Total_Splicing_Burden <- as.data.frame(transposed_Total)
+  total_results = transform(merge(all_oe_results_df, all_ue_results_df, by = 0, all = TRUE), TotalOutliers = OutlierNumberOver + OutlierNumberUnder)
+  rownames(total_results) <- total_results[, 1]
+  convert_total <- subset(total_results, select = c(-OutlierNumberOver, -OutlierNumberUnder, -Row.names))
+  total_burden <- as.data.frame(convert_total)
 
   detach("package:dplyr")
 
-  return(splice_burden <- cbind(Over_Expressed_Splicing_Burden, Under_Expressed_Splicing_Burden, Total_Splicing_Burden))
+  return(splice_burden <- cbind(all_oe_results_df, all_ue_results_df, total_burden))
 }
