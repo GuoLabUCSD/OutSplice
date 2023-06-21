7b2ea27ef2fa9125809ef0959297fcfa5c03f548
GuoLabUCSD <108438457+GuoLabUCSD@users.noreply.github.com>
2023-06-21 13:36:25 -0700

    Add files via upload

--- R/subset_overEx.R
+++ R/subset_overEx.R
@@ -6,9 +6,7 @@ subset_overEx <- function(FisherAnalyses, p_value) {
         toplist90 <- toplist90[toplist90[, "FisherP2"] < p_value, ]
         toplist90 <- subset(toplist90, select = FisherP2)
     } else {
-        toplist90 <- toplist90[toplist90[, "FisherP2"] > p_value, ]
-        toplist90 <- subset(toplist90, select = FisherP2)
-        message("no over expression outliers")
+        toplist90 <- NULL
     }
     return(toplist90)
 }
--- R/subset_underEx.R
+++ R/subset_underEx.R
@@ -6,9 +6,7 @@ subset_underEx <- function(FisherAnalyses, p_value) {
         toplist10 <- toplist10[toplist10[, "FisherP1"] < p_value, ]
         toplist10 <- subset(toplist10, select = FisherP1)
     } else {
-        toplist10 <- toplist10[toplist10[, "FisherP1"] > p_value, ]
-        toplist10 <- subset(toplist10, select = FisherP1)
-        message("no under expression outliers")
+        toplist10 <- NULL
     }
     return(toplist10)
 }
